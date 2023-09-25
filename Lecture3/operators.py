e238aaa44f7640c7b9c5949e78e01d98c34e8898
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-09-25 10:51:38 +0530

    lecture3 about operators

---
+++ Lecture3/operators.py
@@ -0,0 +1,14 @@
+# 1. Operators in Python 
+
+#  Arithmatic -> + - * / // % ** 
+#  Comparison -> > < >= <= ==
+#  Logical -> and or not 
+#  Assignment -> =
+#  Identity -> is is not
+#  Membership -> in not in
+#  Bitwise  -> & | ^ ~ << >> 
+
+
+# Ternary operator in py 
+meaning  = 12
+print(meaning) if meaning >= 12 else print("wrong")
