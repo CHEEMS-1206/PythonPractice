14afe5a5b641a320d2018816bcaab3ab00b473b6
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-09-25 10:26:19 +0530

    lecture2 about variables, comments and indentations

---
+++ Lecture2/welcome.py
@@ -0,0 +1,15 @@
+# 1. variables and naming conventions
+# 2. expressions and statements 
+# 3. comments 
+# 4. indentation in python and auto formatiing by vs code 
+
+headAndFoot = "********************"
+centerBody = "*                  *"
+mainBody = "*     WELCOME!     *"
+
+print(' ')
+print(headAndFoot)
+print(centerBody)
+print(mainBody)
+print(centerBody)
+print(headAndFoot)
