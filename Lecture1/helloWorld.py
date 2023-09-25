3b903323634b69b9677c8d4144ae3bda25ea089a
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-09-25 10:04:40 +0530

    created and executed first python file, and completed lecture1

---
+++ Lecture1/helloWorld.py
@@ -0,0 +1,8 @@
+# 1. Install python 
+# 2. Install Python Extentions
+# 3. REPL = Read Evaluate Print Look 
+# 4. High level Python
+# 5. Create Run and Execute python file 
+
+greetings = "Hello world!"
+print(greetings)
